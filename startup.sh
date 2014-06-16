c3e5f8b3914f0a71f83822a265ef8360cee163f2
Phyks <webmaster@phyks.me>
2014-06-16 15:34:16 +0200

    Rename jarvis

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ until ping -c 4 hackens.org > /dev/null 2>&1; do
     sleep 2
 done
 
-screen -dmS jarvis && screen -S jarvis -p 0 -X stuff "~/Jarvis/jarvis_irc.py$(printf \\r)"
+screen -dmS jarvis && screen -S jarvis -p 0 -X stuff "~/Jarvis/jarvis_bot.py$(printf \\r)"
