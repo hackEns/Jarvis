d8ac0a16caa13072f0d2da609eb42f2d33c23a1d
Phyks <webmaster@phyks.me>
2014-06-16 15:32:29 +0200

    Starting refactor

--- tests/cam_control.py
+++ tests/cam_control.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-import random
-import time
 import os
 import sys
-import colorsys
 import serial
 
 path = "/dev/ttyACM1"
 
 angle = int(sys.argv[1])
 if angle < 0 or angle > 180:
-	print "Angle invalide"
-	raise SystemExit
+    print "Angle invalide"
+    raise SystemExit
 
 os.system("stty -hup -F "+path)
 
 try:
-	ser = serial.Serial(path, 115200)
+    ser = serial.Serial(path, 115200)
 except:
-	sys.exit("Erreur à l'ouverture du port série.")
+    sys.exit("Erreur à l'ouverture du port série.")
 
 towrite = chr(127+int(127*float(angle)/180))
 ser.readline()
